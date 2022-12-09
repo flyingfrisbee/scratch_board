82672ebd0a7b00e66403074b7581492abd064786
dera1337 <yoris.mourinho@yahoo.com>
2022-12-09 21:24:43 +0700

    calculator halfway done

--- main.go
+++ main.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hi everybody 2")
+	// pen bisa tambah krg dsb + - & / * yes
+	// fmt.Scanln() buat nerima input string & operator yes
+	// cari tau operator yang dipanggil apa (if)
+	// string di convert to int and float64
+	// return result fmt.println
+	// semua harus di loop
+
+	var angka1 string
+	var angka2 string
+	var operator string
+
+	fmt.Println("masukkan angka pertama: ")
+	fmt.Scanln(&angka1)
+
+	fmt.Println("masukkan angka kedua: ")
+	fmt.Scanln(&angka2)
+
+	fmt.Println("masukkan operator: ")
+	fmt.Scanln(&operator)
+
+	switch operator {
+	case "+":
+		angkaSatu, err := strconv.Atoi(angka1)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+
+		angkaDua, err := strconv.Atoi(angka2)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+		fmt.Println(add(angkaSatu, angkaDua))
+
+	case "-":
+		angkaSatu, err := strconv.Atoi(angka1)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+
+		angkaDua, err := strconv.Atoi(angka2)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+		fmt.Println(substract(angkaSatu, angkaDua))
+	case "%":
+		angkaSatu, err := strconv.Atoi(angka1)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+
+		angkaDua, err := strconv.Atoi(angka2)
+		if err != nil { //if error is not non exist
+			log.Println(err)
+			return
+		}
+		fmt.Println(modulus(angkaSatu, angkaDua))
+
+	case "/":
+		fmt.Println("divide")
+	case "*":
+		fmt.Println("multiply")
+	}
+}
+
+func add(value1, value2 int) int {
+	return value1 + value2
+}
+
+func substract(value1, value2 int) int {
+	return value1 - value2
+}
+
+func modulus(value1, value2 int) int {
+	return value1 % value2
+}
+
+func multiply(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divide(value1, value2 float64) float64 {
+	return value1 / value2
 }
